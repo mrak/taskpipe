9bda61844ebb56dd903f3b866c875a43ebb391a0
Eric Mrak <mail@ericmrak.info>
2015-01-28 20:05:39 -0800

    Making Pipe generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,35 @@
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{Receiver,Sender,channel};
 use std::io::{ChanReader,ChanWriter};
 use std::thread::Thread;
 
-struct Pipe {
-    incoming: ChanReader
+struct Pipe<T: Send> {
+    rx: Receiver<T>
 }
 
-impl Pipe {
-    fn source(source: &mut Buffer) -> Pipe {
-        let (tx, rx) = channel();
-        let reader = ChanReader::new(rx);
-        let mut writer = ChanWriter::new(tx);
-
-        loop {
-            match source.read_char() {
-                Ok(c) => writer.write_char(c),
-                Err(_) => break
-            };
-        };
-
-        Pipe { incoming: reader }
-    }
-
-    fn sink(&mut self, sink: &mut Writer) {
-        loop {
-            match self.incoming.read_char() {
-                Ok(c) => sink.write_char(c),
-                Err(_) => break
-            };
-        };
-    }
-
-    fn pipe<F: FnOnce(&mut ChanReader, &mut ChanWriter)+Send>(mut self, transform: F) -> Pipe {
-        let (tx, rx) = channel();
-        let reader = ChanReader::new(rx);
-        let mut writer = ChanWriter::new(tx);
+impl<T: Send> Pipe<T> {
+    fn tap<F: FnOnce(&mut Sender<T>)+Send>(task: F) -> Pipe<T> {
+        let (mut tx, rx) = channel();
 
         Thread::spawn(move || {
-            transform(&mut self.incoming, &mut writer);
+            task(&mut tx);
         });
 
-        Pipe { incoming: reader }
+        Pipe { rx: rx }
+    }
+
+    fn drain<F: FnOnce(&mut Receiver<T>)+Send>(mut self, task: F) {
+        Thread::spawn(move || {
+            task(&mut self.rx);
+        });
+    }
+
+    fn pipe<F: FnOnce(&mut Receiver<T>, &mut Sender<T>)+Send>(mut self, task: F) -> Pipe<T> {
+        let (mut tx, rx) = channel();
+
+        Thread::spawn(move || {
+            task(&mut self.rx, &mut tx);
+        });
+
+        Pipe { rx: rx }
     }
 }
