a887e12b47acd70c4845c4472e546bb4ae348389
Eric Mrak <mail@ericmrak.info>
2015-01-28 20:51:35 -0800

    Rename to taskpipe, add pump function

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,46 @@
 use std::sync::mpsc::{Receiver,Sender,channel};
-use std::io::{ChanReader,ChanWriter};
 use std::thread::Thread;
 
-struct Pipe<T: Send> {
-    rx: Receiver<T>
+pub struct TaskPipe<T: Send> {
+    outlet: Receiver<T>
 }
 
-impl<T: Send> Pipe<T> {
-    fn tap<F: FnOnce(&mut Sender<T>)+Send>(task: F) -> Pipe<T> {
+impl<T: Send> TaskPipe<T> {
+    pub fn tap<F: FnOnce(&mut Sender<T>)+Send>(task: F) -> TaskPipe<T> {
         let (mut tx, rx) = channel();
 
         Thread::spawn(move || {
             task(&mut tx);
         });
 
-        Pipe { rx: rx }
+        TaskPipe { outlet: rx }
     }
 
-    fn drain<F: FnOnce(&mut Receiver<T>)+Send>(mut self, task: F) {
-        Thread::spawn(move || {
-            task(&mut self.rx);
-        });
-    }
-
-    fn pipe<F: FnOnce(&mut Receiver<T>, &mut Sender<T>)+Send>(mut self, task: F) -> Pipe<T> {
+    pub fn pipe<F: FnOnce(&mut Receiver<T>, &mut Sender<T>)+Send>(mut self, task: F) -> TaskPipe<T> {
         let (mut tx, rx) = channel();
 
         Thread::spawn(move || {
-            task(&mut self.rx, &mut tx);
+            task(&mut self.outlet, &mut tx);
         });
 
-        Pipe { rx: rx }
+        TaskPipe { outlet: rx }
     }
+
+    pub fn drain<F: FnOnce(&mut Receiver<T>)+Send>(mut self, task: F) {
+        Thread::spawn(move || {
+            task(&mut self.outlet);
+        });
+    }
+}
+
+pub fn pump<T: Send>(rx: &Receiver<T>, tx: &Sender<T>) {
+    let tx2 = tx.clone();
+    Thread::spawn(move || {
+        for message in rx.iter() {
+            match tx2.send(message) {
+                Err(_) => return,
+                Ok(_) => continue
+            }
+        }
+    });
 }
