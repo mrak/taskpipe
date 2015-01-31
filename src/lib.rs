e3d793b674b3df60fdc645980b870dd0e08e3f78
Eric Mrak <mail@ericmrak.info>
2015-01-30 21:58:32 -0800

    Adding some rustdoc for input

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
+//! This is a small library that simplifies the process of chaining together several
+//! sub-tasks via channels. Each sub-task will be executed in it's own thread and
+//! given a read channel as well as a write channel.
+
 use std::sync::mpsc::{Receiver,Sender,channel};
 use std::thread::Thread;
 
+/// A taskpipe continuation
+///
+/// This struct is not meant to be used directly. Instead, it the return type of other
+/// functions within this library
 pub struct TaskPipe<T: Send> {
+    /// The reading end of the previous task's channel
     rx: Receiver<T>
 }
 
+/// Begin a taskpipe with a production function
+///
+/// This is intended for the beginning of a task pipeline. The supplied function
+/// will be used to feed the rest of the pipeline.
+/// ```
+/// taskpipe::input(|tx: Sender<char>| {
+///     for c in "abcdefghijklmnopqrstuvwxyz".chars() {
+///         tx.send(c);
+///     }
+/// });
+/// ```
 pub fn input<T: Send, F: FnOnce(Sender<T>)+Send>(task: F) -> TaskPipe<T> {
     let (tx, rx) = channel();
 
@@ -16,6 +36,18 @@ pub fn input<T: Send, F: FnOnce(Sender<T>)+Send>(task: F) -> TaskPipe<T> {
 }
 
 impl<T: Send> TaskPipe<T> {
+    /// Insert additional items into the pipeline
+    ///
+    /// Items from the previous task will be passed through to the next task.
+    /// Items produced by the given function will also be received by
+    /// downstream tasks.
+    /// ```
+    /// taskpipe::input(|tx: Sender<char>| {
+    ///     for c in "abcdefghijklmnopqrstuvwxyz".chars() { tx.send(c); }
+    /// }).input(|tx: Sender<char>| {
+    ///     for c in "0123456789".chars() { tx.send(c); }
+    /// });
+    /// ```
     pub fn input<F: FnOnce(Sender<T>)+Send>(self, task: F) -> TaskPipe<T> {
         let (tx, rx) = channel();
         let mut tx2 = tx.clone();
@@ -32,6 +64,10 @@ impl<T: Send> TaskPipe<T> {
         TaskPipe { rx: rx }
     }
 
+    /// Transform or process pipeline items
+    ///
+    /// Received upstream task items, process them, and pass them along to
+    /// downstream tasks.
     pub fn pipe<S: Send, F: FnOnce(Receiver<T>, Sender<S>)+Send>(self, task: F) -> TaskPipe<S> {
         let (tx, rx) = channel();
 
@@ -42,6 +78,10 @@ impl<T: Send> TaskPipe<T> {
         TaskPipe { rx: rx }
     }
 
+    /// Finalize the pipeline, consuming any items.
+    ///
+    /// This method joins the final task to the parent's thread, while receiving
+    /// any items that have made their way through the entire pipeline.
     pub fn output<F: FnOnce(Receiver<T>)+Send>(self, task: F) {
         Thread::scoped(move || {
             task(self.rx);
